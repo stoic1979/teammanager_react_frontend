e20d18873aaee97a8468ed1696cb22706d06cece
Navjot Singh <weavebytes@gmail.com>
2017-12-06 19:21:10 +0530

    adding config/settings for project

---
+++ src/config/settings.js
@@ -0,0 +1,12 @@
+const settings = {
+
+    API_ROOT: process.env.TEAMMANAGER_API_ROOT,
+
+    // User roles
+    ADMINISTRATOR: 'ADMIN',
+    MANAGER: 'moderator',
+    WORKER: 'worker',
+
+};
+
+export default settings;
